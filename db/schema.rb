f214cdd12bdf92ae2124705a284fb7261a47d0d9
Marcel <marceli99@protonmail.com>
2020-06-26 23:20:00 +0200

    feat(settings): add settings model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_173526) do
+ActiveRecord::Schema.define(version: 2020_06_26_211821) do
+
+  create_table "settings", force: :cascade do |t|
+    t.text "short_description"
+    t.text "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
