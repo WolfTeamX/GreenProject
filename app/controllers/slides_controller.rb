1e48f12c221e1c336feca5ab08e7b9dd6fe2d0d7
Jacob <kanewers@gmail.com>
2020-07-09 11:18:30 +0200

    fix(slides_controller): fixed redirection

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -8,13 +8,13 @@ class SlidesController < ApplicationController
     slide.content = params[:content]
     slide.image.attach(params[:image])
     slide.save
-    redirect_back(fallback_location: admin_index_path)
+    redirect_to admin_index_path
   end
 
   def destroy
     slide = Slide.find(params[:format])
     slide.destroy
-    redirect_to(admin_index_path)
+    redirect_to admin_index_path
   end
 
   def manage
