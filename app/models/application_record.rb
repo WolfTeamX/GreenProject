584ba2f91d32395c1066e1dc4c3c8a3dfcba91b7
Jacob <kanewers@gmail.com>
2020-07-27 14:30:17 +0200

    update(application_record.rb): add placeholder to models with images

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  after_commit :set_placeholder, on: [:create]
+
+  def set_placeholder
+    if defined?(image)
+      unless self.image.attached?
+        self.image.attach(io: File.open(Rails.root.join("app", "assets", "images", "placeholder.png")), filename: 'placeholder.png' , content_type: "image/png")
+      end
+    end
+  end
 end
