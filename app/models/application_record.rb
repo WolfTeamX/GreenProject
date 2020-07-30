c4e366f4414a3e032fb58740663e6ace2cda7916
Jacob <kanewers@gmail.com>
2020-07-30 22:42:15 +0200

    feat(application_record.rb): add image compression

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,12 +1,32 @@
 class ApplicationRecord < ActiveRecord::Base
+  require 'mini_magick'
   self.abstract_class = true
 
   after_commit :set_placeholder, on: [:create]
+  after_commit :compress_image, on: [:create, :update]
 
   def set_placeholder
-    if defined?(image)
+    if defined? image
       unless self.image.attached?
-        self.image.attach(io: File.open(Rails.root.join("app", "assets", "images", "placeholder.png")), filename: 'placeholder.png' , content_type: "image/png")
+        @minified = true
+        self.image.attach(
+            io: File.open(Rails.root.join("app", "assets", "images", "placeholder.png")),
+            filename: 'placeholder.png',
+            content_type: "image/png")
+      end
+    end
+  end
+
+  def compress_image
+    if defined? image
+      if self.image.attached?
+        unless @minified
+          processed = ImageProcessing::MiniMagick
+                          .source(ActiveStorage::Blob.service.path_for(image.key))
+                          .resize_to_limit(1920, 1920)
+                          .call
+          @minified = true
+        end
       end
     end
   end
