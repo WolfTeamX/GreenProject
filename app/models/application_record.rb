9f11e9ccb7572fe006e6103f3dc39dae68e36df1
Jacob <kanewers@gmail.com>
2020-07-30 23:09:05 +0200

    fix(application_record.rb): image compression

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -8,7 +8,6 @@ class ApplicationRecord < ActiveRecord::Base
   def set_placeholder
     if defined? image
       unless self.image.attached?
-        @minified = true
         self.image.attach(
             io: File.open(Rails.root.join("app", "assets", "images", "placeholder.png")),
             filename: 'placeholder.png',
@@ -20,13 +19,15 @@ class ApplicationRecord < ActiveRecord::Base
   def compress_image
     if defined? image
       if self.image.attached?
-        unless @minified
-          processed = ImageProcessing::MiniMagick
-                          .source(ActiveStorage::Blob.service.path_for(image.key))
-                          .resize_to_limit(1920, 1920)
-                          .call
-          @minified = true
+        if @compressed_image || self.image.blob.content_type == "image/svg+xml"
+          return
         end
+        @compressed_image = true
+        processed = ImageProcessing::MiniMagick
+                        .source(ActiveStorage::Blob.service.path_for(image.key))
+                        .call
+        .resize_to_limit(1920, 1920)
+        self.image.attach(io: processed, filename: self.image.blob.filename, content_type: self.image.blob.content_type)
       end
     end
   end
